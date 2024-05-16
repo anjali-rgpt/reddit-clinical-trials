3de8cb711b54f44b6b7c570365f8a43f42c2fe81
Anjali Ragupathi <anjur99@gmail.com>
2024-05-16 04:12:05 -0700

    Commentary

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -20,28 +20,35 @@ def search(keywords):
     subreddits = set()
 
     for k in [keywords, keywords + "health conditions", keywords + "health conditions" + "clinical trial", keywords + "clinical trial"]:
+        
+        # First, we search by name and keyword for different kinds of queries
         name_search = reddit.subreddits.search_by_name(k)[:3]
         all_search = list(reddit.subreddits.search(k))[:3]
         # print("\n", name_search, all_search)
 
         for element in name_search + all_search:
             subreddits.add(element)
+
+        # include posts from subreddits we don't actually specifically search for 
         subreddits.add(all)
     
     # print("\n", subreddits)
 
     for subreddit in subreddits:
 
+        #search within subreddits
         check = subreddit.search("clinical trials interest " + keywords, sort = "relevance")
         post_ids = [x.id for x in list(check)][:50]
-        print("CHECK")
+    
         for post_id in post_ids:
+            # Store post data for every relevant user
             submission = reddit.submission(post_id)
             try:
                 users[submission.author.name].append(submission.selftext)
             except:
                 users[submission.author.name] = [submission.selftext]
 
+            # Go through comments for each post to find other interested users
             submission.comment_sort = "top"
             submission.comments.replace_more(limit=7)
             for comment in submission.comments.list():
@@ -55,7 +62,7 @@ def search(keywords):
                     except:
                         continue
 
-
+    print("Reddit Data Scraped")
     return users
 
 
